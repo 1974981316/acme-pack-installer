0c25c291f2274ad0e3f86000a8cbceb406c4cfaf
张峻铭 <1974981316@qq.com>
2020-08-23 17:06:29 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+packdir=/sdcard/games/com.mojang/resource_packs/ACMEPack
+cp -r ./src/a/* $packdir
+cp -r ./src/d/* $packdir
+cp -r ./src/f/* $packdir
+cp -r ./src/l/* $packdir
+cp -r ./src/m/* $packdir
+cp -r ./src/p/* $packdir
+cp -r ./src/s/* $packdir
+cp -r ./src/t/* $packdir
+cp -r ./src/u/* $packdir
